16e7a87b24f7fde40895494fe0785d67ae59f9bd
David Pinheiro <davidmbp57@gmail.com>
2024-03-04 14:55:28 +0100

    Update simpleOnionRouter.ts

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -11,6 +11,18 @@ export async function simpleOnionRouter(nodeId: number) {
     res.send("live"); 
   });
 
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.status(200).json({ result: null });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.status(200).json({ result: null });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.status(200).json({ result: null});
+  });
+
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
       `Onion router ${nodeId} is listening on port ${
